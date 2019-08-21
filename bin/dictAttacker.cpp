48ff26f7179347c409aace4136760d11064e7261
Marios Konidaris <fanatika.f1@gmail.com>
2019-08-21 23:22:20 +0300

    Implemented mode 2 of the program. First it checks if the word from the dictionary starts from a number and ends with a number, if yes, it skips. Else, it capitalizes the first character, it checks the capitalizes the last character, it checks, and lastly it capitalizes the two charactes and checks. A better implementation is on the way.

--- bin/dictAttacker.cpp
+++ bin/dictAttacker.cpp
@@ -13,19 +13,21 @@
 
 int main(int argc, char const *argv[]) {
   unsigned int results;
-  auto start = std::chrono::high_resolution_clock::now();
 
   if (argc >= 3) {
+    auto start = std::chrono::high_resolution_clock::now();
+    
     Attacker attack(argv[1], argv[2], (unsigned short) std::strtoul(argv[3], NULL, 0));
     attack.loadDictionary();
     results = attack.crack();
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsedSeconds = end - start;
+    std::cout << "Cracked " << results << "." << std::endl;
+    std::cout << "\nTook: " << elapsedSeconds.count() << "s." << std::endl;
   } else
       std::cout << "Execution: ./dictAttacker <hashedPasswordsPath> <dictionaryPath> <mode>" << std::endl;
 
-  auto end = std::chrono::high_resolution_clock::now();
-
-  std::chrono::duration<double> elapsedSeconds = end - start;
-  std::cout << "Cracked " << results << "." << std::endl;
-  std::cout << "\nTook: " << elapsedSeconds.count() << "s." << std::endl;
   return 0;
 }
--- core/attacker.cpp
+++ core/attacker.cpp
@@ -2,6 +2,8 @@
 #include "sha512.hh"
 
 #include <fstream>
+#include <cctype>
+#include <regex>
 
 Attacker::Attacker(std::string ph, std::string pd, unsigned short int m) {
   pathToHash = ph;
@@ -34,7 +36,7 @@ unsigned int Attacker::crack() {
   switch (mode) {
     default:
     case 1:
-      std::cout << "Executing by checking with every word in the dictionary." << std::endl;
+      std::cout << "[Mode 1] Every word in the dictionary is being hashed and compared to the passwords." << std::endl;
 
       if (input.is_open()) {
         while (!input.eof()) {
@@ -47,10 +49,46 @@ unsigned int Attacker::crack() {
           }
         }
       }
+
       break;
-    case 2:
-      std::cout << "Executed case 2." << std::endl;
+    case 2: {
+      const std::regex ch("[0-9].*[0-9]");
+      std::cout << "[Mode 2] Every word in the dictionary is being manipulated by first capitalizing the first and then the last character." << std::endl;
+
+      if (input.is_open()) {
+        while (!input.eof()) {
+          std::getline(input, tmp);
+          for (auto &word: dictionary) {
+            if (std::regex_match(word, ch))
+              continue;
+            else {
+              //First capitalize the first character of each word
+              word.front() = toupper(word.front());
+              if (!sw::sha512::calculate(word).compare(tmp)) {
+                numOfCracked++;
+                std::cout << tmp << " -> " << word << std::endl;
+              }
+              //Then capitalize the last character of each word
+              word.front() = tolower(word.front());
+              word.back() = toupper(word.back());
+              if (!sw::sha512::calculate(word).compare(tmp)) {
+                numOfCracked++;
+                std::cout << tmp << " -> " << word << std::endl;
+              }
+              //Finally capitalize the first and last character of each word
+              word.front() = toupper(word.front());
+              word.back() = toupper(word.back());
+              if (!sw::sha512::calculate(word).compare(tmp)) {
+                numOfCracked++;
+                std::cout << tmp << " -> " << word << std::endl;
+              }
+            }
+          }
+        }
+      }
+
       break;
+    }
   }
 
   input.close();
